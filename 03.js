dce2ce383884cfb32fd0a7d81b0b09ee24204409
seba2803 <sebaovelazquez@gmail.com>
2022-11-15 14:36:10 -0300

    terminado

--- 03.js
+++ 03.js
@@ -14,9 +14,14 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
-  
+  let amigo = {}
+  amigos.forEach(function(item){
+    if(nombre == item.nombre){
+      amigo = item
+    }
+  })
+  return amigo;
 }
-
 // No modifiques nada debajo de esta linea //
 
 module.exports = buscarAmigo
